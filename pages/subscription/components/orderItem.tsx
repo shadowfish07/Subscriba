56d3738b2fd1f45b789af8711b268e7093dbc6c9
shadowfish <1633742366@qq.com>
2023-10-31 22:53:12 +0800

    订单添加结束时间

--- pages/subscription/components/orderItem.tsx
+++ pages/subscription/components/orderItem.tsx
@@ -3,6 +3,7 @@ import { DraftOrder, OrderType } from "../../../types";
 import { formatDate } from "../../../util/formatDate";
 import { View } from "react-native";
 import { rawTimeExtension2String } from "../../../util/rawTimeExtension2String";
+import { TimeExtension } from "../../../util/timeExtension";
 
 type Props = {
   draftOrder: DraftOrder;
@@ -10,6 +11,18 @@ type Props = {
 
 export const OrderItem = ({ draftOrder }: Props) => {
   const theme = useTheme();
+
+  const endAt = (function () {
+    if (draftOrder.type === OrderType.Buyout) {
+      return "";
+    }
+
+    return formatDate(
+      draftOrder.orderDate +
+        new TimeExtension(draftOrder.timeExtension).millisecond
+    );
+  })();
+
   return (
     <Card style={{ backgroundColor: "rgba(73, 69, 79, 1)", marginBottom: 10 }}>
       <Card.Content>
@@ -21,12 +34,13 @@ export const OrderItem = ({ draftOrder }: Props) => {
             )}
             <Text variant="labelSmall">{formatDate(draftOrder.orderDate)}</Text>
           </View>
-          <View style={{ justifyContent: "center" }}>
+          <View style={{ justifyContent: "center", alignItems: "flex-end" }}>
             <Text variant="titleMedium" style={{ color: theme.colors.primary }}>
               {draftOrder.type === OrderType.Buyout
                 ? "永久"
                 : "+" + rawTimeExtension2String(draftOrder.timeExtension)}
             </Text>
+            {endAt !== "" && <Text variant="labelSmall">{endAt}</Text>}
           </View>
         </View>
       </Card.Content>
