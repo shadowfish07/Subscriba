ba733fddc1d8d3bf3ace3239b66b76fc59c6d17d
shadowfish <1633742366@qq.com>
2023-11-12 16:34:50 +0800

    wip

--- lib/src/order/order_card.dart
+++ lib/src/order/order_card.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
 
 class OrderCard extends StatelessWidget {
-  const OrderCard({super.key});
+  const OrderCard({super.key, this.color});
 
+  final Color? color;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Card(
+        color: color,
         child: SizedBox(
           height: 80,
           child: Padding(
             padding: const EdgeInsets.only(left: 16, right: 16),
             child: Center(
-                child: Text(
-              "Order",
-              style: Theme.of(context).textTheme.titleMedium,
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Annual subscription",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(
+                      "02/02/2012 - 02/03/2012",
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                          color:
+                              Theme.of(context).colorScheme.onSurfaceVariant),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "\$780",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(
+                      "+ 30 days",
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                          color:
+                              Theme.of(context).colorScheme.onSurfaceVariant),
+                    )
+                  ],
+                ),
+              ],
             )),
           ),
         ),
--- lib/src/subsciption_detail/subsciption_detail_view.dart
+++ lib/src/subsciption_detail/subsciption_detail_view.dart
@@ -13,215 +13,233 @@ class SubscriptionDetailView extends StatelessWidget {
           foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
           backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              clipBehavior: Clip.none,
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SubscriptionDetailHeader(),
-                Positioned(
-                    left: 24,
-                    right: 24,
-                    top: 90,
-                    child: Card(
-                      color: Theme.of(context).colorScheme.surfaceVariant,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            top: 8, bottom: 8, left: 16, right: 16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    SubscriptionDetailHeader(),
+                    Positioned(
+                        left: 24,
+                        right: 24,
+                        top: 90,
+                        child: Card(
+                          color: Theme.of(context).colorScheme.surfaceVariant,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 8, bottom: 8, left: 16, right: 16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Text(
-                                      "Subscribed for 400 days",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleSmall,
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Subscribed for 400 days",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleSmall,
+                                        ),
+                                        Text(
+                                          "Expires in 3 days", // Or Buyout
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelMedium!
+                                              .copyWith(
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .onSurfaceVariant),
+                                        ),
+                                      ],
                                     ),
-                                    Text(
-                                      "Expires in 3 days", // Or Buyout
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .labelMedium!
-                                          .copyWith(
-                                              color: Theme.of(context)
-                                                  .colorScheme
-                                                  .onSurfaceVariant),
-                                    ),
+                                    Chip(
+                                        backgroundColor: Theme.of(context)
+                                            .colorScheme
+                                            .surfaceVariant,
+                                        label: Text("90%",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .labelMedium))
                                   ],
                                 ),
-                                Chip(
-                                    backgroundColor: Theme.of(context)
-                                        .colorScheme
-                                        .surfaceVariant,
-                                    label: Text("90%",
+                                const SizedBox(height: 24),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text("11/01/2012",
                                         style: Theme.of(context)
                                             .textTheme
-                                            .labelMedium))
+                                            .labelMedium!
+                                            .copyWith(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .onSurfaceVariant)),
+                                    Text("11/01/2012",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelMedium!
+                                            .copyWith(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .onSurfaceVariant))
+                                  ],
+                                ),
+                                const SizedBox(height: 8),
+                                LinearProgressIndicator(
+                                  value: 0.8,
+                                )
                               ],
                             ),
-                            const SizedBox(height: 24),
-                            Row(
+                          ),
+                        ))
+                  ],
+                ),
+                SizedBox(height: 54),
+                Padding(
+                  padding: defaultCenterPadding,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          child: DisplayCard(
+                        title: Text(
+                          "Daily ",
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        body: Text(
+                          "\$900",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      )),
+                      Expanded(
+                          child: DisplayCard(
+                        title: Text(
+                          "Monthly ",
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        body: Text(
+                          "\$900",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      )),
+                      Expanded(
+                          child: DisplayCard(
+                        title: Text(
+                          "Yearly ",
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        body: Text(
+                          "\$900",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      )),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: defaultCenterPadding,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          flex: 2,
+                          child: DisplayCard(
+                            title: Text(
+                              "Totally Cost",
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                            body: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Text("11/01/2012",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .labelMedium!
-                                        .copyWith(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .onSurfaceVariant)),
-                                Text("11/01/2012",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .labelMedium!
-                                        .copyWith(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .onSurfaceVariant))
+                                Text(
+                                  "\$900",
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                ),
+                                Text("Top 1",
+                                    style:
+                                        Theme.of(context).textTheme.labelMedium)
                               ],
                             ),
-                            const SizedBox(height: 8),
-                            LinearProgressIndicator(
-                              value: 0.8,
-                            )
-                          ],
+                          )),
+                      Expanded(
+                          child: DisplayCard(
+                        title: Text(
+                          "Orders",
+                          style: Theme.of(context).textTheme.bodyMedium,
                         ),
+                        body: Text(
+                          "9",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      )),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: defaultCenterPadding,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          child: DisplayCard(
+                        isClickable: true,
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                        title: Text(
+                          "Renew",
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        body: Text(
+                          "On",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      )),
+                      Expanded(
+                          flex: 2,
+                          child: DisplayCard(
+                            title: Text(
+                              "Next Payment",
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                            body: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "\$900",
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                ),
+                                Text("In 3 days",
+                                    style:
+                                        Theme.of(context).textTheme.labelMedium)
+                              ],
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 8),
+                Section(
+                    title: "Orders",
+                    child: Column(children: [
+                      OrderCard(
+                        color: Theme.of(context).colorScheme.surfaceVariant,
                       ),
-                    ))
+                      OrderCard(
+                        color: Theme.of(context).colorScheme.surfaceVariant,
+                      )
+                    ]))
               ],
             ),
-            SizedBox(height: 54),
-            Padding(
-              padding: defaultCenterPadding,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                      child: DisplayCard(
-                    title: Text(
-                      "Daily ",
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    body: Text(
-                      "\$900",
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  )),
-                  Expanded(
-                      child: DisplayCard(
-                    title: Text(
-                      "Monthly ",
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    body: Text(
-                      "\$900",
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  )),
-                  Expanded(
-                      child: DisplayCard(
-                    title: Text(
-                      "Yearly ",
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    body: Text(
-                      "\$900",
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  )),
-                ],
-              ),
-            ),
-            Padding(
-              padding: defaultCenterPadding,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                      flex: 2,
-                      child: DisplayCard(
-                        title: Text(
-                          "Totally Cost",
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        body: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "\$900",
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                            Text("Top 1",
-                                style: Theme.of(context).textTheme.labelMedium)
-                          ],
-                        ),
-                      )),
-                  Expanded(
-                      child: DisplayCard(
-                    title: Text(
-                      "Orders",
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    body: Text(
-                      "9",
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  )),
-                ],
-              ),
-            ),
-            Padding(
-              padding: defaultCenterPadding,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                      child: DisplayCard(
-                    isClickable: true,
-                    color: Theme.of(context).colorScheme.primaryContainer,
-                    title: Text(
-                      "Renew",
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    body: Text(
-                      "On",
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  )),
-                  Expanded(
-                      flex: 2,
-                      child: DisplayCard(
-                        title: Text(
-                          "Next Payment",
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        body: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "\$900",
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                            Text("In 3 days",
-                                style: Theme.of(context).textTheme.labelMedium)
-                          ],
-                        ),
-                      )),
-                ],
-              ),
-            ),
-            SizedBox(height: 8),
-            Section(
-                title: "Orders", child: ListView(children: const [OrderCard()]))
-          ],
+          ),
         ));
   }
 }
