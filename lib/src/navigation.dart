e55870d6d66cdc75ed68fc4a8b3c4d6125000db1
shadowfish <1633742366@qq.com>
2024-03-02 22:02:18 +0800

    feat: refactor code to use Provider package for state management
    The code has been refactored to use the Provider package for state management. The `subscriptionsPageModel` has been moved to a separate file `subscriptions_page_model.dart` and is now created using the `Provider` widget. The `SubscriptionsView` and `HomeAppBar` classes have been updated to use the `Provider` widget and access the `subscriptionsPageModel` and `subscriptionsModel` respectively. This allows for better separation of concerns and improved code organization.

--- lib/src/navigation.dart
+++ lib/src/navigation.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:subscriba/src/home/home_view.dart';
 import 'package:subscriba/src/store/subscriptions_model.dart';
+import 'package:subscriba/src/subscriptions/subscriptions_page_model.dart';
 import 'package:subscriba/src/subscriptions/subscriptions_view.dart';
+
+final subscriptionsPageModel = SubscriptionPageModel();
 
 class Navigation extends StatefulWidget {
   const Navigation({super.key});
@@ -25,33 +28,37 @@ class _NavigationState extends State<Navigation> {
   @override
   Widget build(BuildContext context) {
     subscriptionModel = Provider.of<SubscriptionsModel>(context);
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.toc),
-            icon: Icon(Icons.toc_outlined),
-            label: 'Subscriptions',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.info),
-            icon: Icon(Icons.info_outline),
-            label: 'About',
-          )
-        ],
-        selectedIndex: currentIndex,
-        onDestinationSelected: setCurrentIndex,
-      ),
-      body: [
-        const HomeView(),
-        const SubscriptionsView(),
-        Container()
-      ][currentIndex],
-    );
+    return Provider(
+        create: (_) => subscriptionsPageModel,
+        builder: (context, child) {
+          return Scaffold(
+            bottomNavigationBar: NavigationBar(
+              destinations: const [
+                NavigationDestination(
+                  selectedIcon: Icon(Icons.home),
+                  icon: Icon(Icons.home_outlined),
+                  label: 'Home',
+                ),
+                NavigationDestination(
+                  selectedIcon: Icon(Icons.toc),
+                  icon: Icon(Icons.toc_outlined),
+                  label: 'Subscriptions',
+                ),
+                NavigationDestination(
+                  selectedIcon: Icon(Icons.info),
+                  icon: Icon(Icons.info_outline),
+                  label: 'About',
+                )
+              ],
+              selectedIndex: currentIndex,
+              onDestinationSelected: setCurrentIndex,
+            ),
+            body: [
+              const HomeView(),
+              const SubscriptionsView(),
+              Container()
+            ][currentIndex],
+          );
+        });
   }
 }
