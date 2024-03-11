5f2806d71da4b12314a7a3f645ca2a8af3e8cb58
shadowfish <1633742366@qq.com>
2024-03-12 03:23:36 +0800

    feat: add feature to check for app updates and download latest version

--- lib/src/about/about_view.dart
+++ lib/src/about/about_view.dart
@@ -1,12 +1,44 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:open_file/open_file.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:subscriba/src/database/model.dart';
 import 'package:subscriba/src/util/file_helper.dart';
+
+// Function to compare two versions
+int compareVersion(String version1, String version2) {
+  List<String> nums1 = version1.split(".");
+  List<String> nums2 = version2.split(".");
+  int n1 = nums1.length, n2 = nums2.length;
+
+  // compare versions
+  int i1, i2;
+  for (int i = 0; i < n1 || i < n2; i++) {
+    i1 = i < n1 ? int.parse(nums1[i]) : 0;
+    i2 = i < n2 ? int.parse(nums2[i]) : 0;
+    if (i1 != i2) {
+      return i1 > i2 ? 1 : -1;
+    }
+  }
+
+  // the versions are equal
+  return 0;
+}
+
+// Function to check if the current version needs to update
+bool needUpdate(String currentVersion, String serverVersion) {
+  // If the current version is less than the server version, return true
+  return compareVersion(currentVersion, serverVersion) == -1;
+}
 
 class AboutView extends StatelessWidget {
   static const routeName = '/about';
@@ -100,18 +132,18 @@ class _Settings extends StatelessWidget {
 
     return Column(
       children: [
+        const _CheckForUpdate(),
         ListTile(
           leading: const Icon(Icons.file_upload),
-          title: const Text('导出数据'),
+          title: const Text('Export data'),
           onTap: () async {
             final data = await BaseModalProvider.export();
-            debugPrint(jsonEncode(data));
             FileHelper.createAndShareTempFile(jsonEncode(data), "output.json");
           },
         ),
         ListTile(
           leading: const Icon(Icons.file_download),
-          title: const Text('导入数据'),
+          title: const Text('Import data'),
           onTap: () async {
             FilePickerResult? result = await FilePicker.platform.pickFiles();
             if (result != null) {
@@ -140,6 +172,218 @@ class _Settings extends StatelessWidget {
           },
         ),
       ],
+    );
+  }
+}
+
+class _CheckForUpdate extends StatefulWidget {
+  const _CheckForUpdate({
+    super.key,
+  });
+
+  @override
+  _CheckForUpdateState createState() => _CheckForUpdateState();
+}
+
+Future<void> downloadFile(
+    String url, String filename, Function(int, int?) onProgress) async {
+  var request = await HttpClient().getUrl(Uri.parse(url));
+  var response = await request.close();
+  var bytes = await consolidateHttpClientResponseBytes(response,
+      onBytesReceived: onProgress);
+  File file = File(filename);
+  await file.writeAsBytes(bytes);
+}
+
+class _CheckForUpdateState extends State<_CheckForUpdate> {
+  bool _isLoading = false;
+  String? latestVersion;
+  double? downloadProgress;
+  String? url;
+  bool isNeedUpdate = false;
+
+  Future<String> downloadAPK() async {
+    safeSetState(() {
+      downloadProgress = 0;
+    });
+    final directory = await getTemporaryDirectory();
+    final path = "${directory.path}/$latestVersion-subscriba.apk";
+    await downloadFile(url!, path, (current, total) {
+      if (total == null) {
+        safeSetState(() {
+          downloadProgress = -1;
+        });
+        return;
+      }
+
+      safeSetState(() {
+        downloadProgress = current / total;
+      });
+    });
+    return path;
+  }
+
+  Future<bool> tryGrantPermission() async {
+    if (await Permission.requestInstallPackages.request().isGranted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> installAPK(String path) async {
+    await OpenFile.open(path);
+    safeSetState(() {
+      downloadProgress = null;
+      isNeedUpdate = false;
+      url = null;
+      latestVersion = null;
+    });
+  }
+
+  void safeSetState(void Function() fn) {
+    if (mounted) {
+      setState(fn);
+    }
+  }
+
+  Future<void> getLatestVersion() async {
+    safeSetState(() {
+      _isLoading = true;
+    });
+
+    var response = await http
+        .get(Uri.https('subscrik-update-uxuaswgvnk.cn-hongkong.fcapp.run'));
+
+    if (response.statusCode == 500) {
+      showSnackBar(
+          'Failed to obtain version information, please try again later.');
+      return;
+    }
+
+    if (response.statusCode == 403) {
+      showSnackBar(
+          'Failed to obtain version information, please try again later.');
+      return;
+    }
+
+    if (response.statusCode == 200) {
+      try {
+        final data = jsonDecode(response.body);
+        final currentVersion = await getAppVersion();
+        safeSetState(() {
+          latestVersion = data['latestVersion'];
+          url = data['url'];
+          isNeedUpdate = latestVersion == null
+              ? false
+              : needUpdate(currentVersion, latestVersion!);
+        });
+      } catch (e) {
+        showSnackBar(
+            'Failed to obtain version information, please try again later.');
+      }
+    }
+
+    safeSetState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLatestVersion();
+  }
+
+  showSnackBar(String message) {
+    if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    onTap() async {
+      if (downloadProgress != null &&
+          downloadProgress! >= 0 &&
+          downloadProgress! < 1) {
+        return;
+      }
+
+      if (url != null &&
+          isNeedUpdate &&
+          latestVersion != null &&
+          downloadProgress != 1) {
+        try {
+          final path = await downloadAPK();
+
+          if (await tryGrantPermission()) {
+            await installAPK(path);
+          } else {
+            showSnackBar('Permission denied');
+          }
+          return;
+        } catch (e) {
+          showSnackBar(
+              'Failed to obtain version information, please try again later.');
+          return;
+        }
+      }
+
+      getLatestVersion();
+    }
+
+    return ListTile(
+      leading: const Icon(Icons.update),
+      title: const Text('Check for updates'),
+      trailing: _isLoading
+          ? const SizedBox(
+              width: 16,
+              height: 16,
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+              ))
+          : downloadProgress != null
+              ? downloadProgress == -1
+                  ? const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("Downloading"),
+                        SizedBox(
+                            width: 16,
+                            height: 16,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                            ))
+                      ],
+                    )
+                  : Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(downloadProgress == 1
+                            ? "Downloaded"
+                            : "Downloading"),
+                        const SizedBox(width: 8),
+                        SizedBox(
+                            width: 16,
+                            height: 16,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                              value: downloadProgress,
+                            ))
+                      ],
+                    )
+              : isNeedUpdate
+                  ? Text('Latest version $latestVersion')
+                  : latestVersion == null
+                      ? null
+                      : const Text("Up to date"),
+      onTap: onTap,
     );
   }
 }
